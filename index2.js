d52f15437c3419143bb9a0081df0af5a1de52b3e
Sean <cd3k@pop-os.localdomain>
2020-01-24 21:16:51 +0000

    initial commit

---
+++ index2.js
@@ -0,0 +1,396 @@
+module.exports = class JsonQL {
+  constructor(schema) {
+    this.schema = schema;
+    this.errors = [];
+    this.fatalError = false;
+
+    this.select = ''
+    this.from = '';
+    this.where = '';
+    this.join = [];
+
+    this.dbTableNames = [];
+  }
+
+  // █▀▀ █▀▀█ █░░█ █▀▀▄   █▀▄▀█ █▀▀ ▀▀█▀▀ █░░█ █▀▀█ █▀▀▄ █▀▀
+  // █░░ █▄▄▀ █░░█ █░░█   █░▀░█ █▀▀ ░░█░░ █▀▀█ █░░█ █░░█ ▀▀█
+  // ▀▀▀ ▀░▀▀ ░▀▀▀ ▀▀▀░   ▀░░░▀ ▀▀▀ ░░▀░░ ▀░░▀ ▀▀▀▀ ▀▀▀░ ▀▀▀
+
+  selectQL(queryObj) {
+
+    this.validateQueryObject(queryObj);
+    this.parseQueryObj(queryObj);
+
+    let select = '';
+    if(this.select.length > 0) {
+      select = `SELECT ${this.select}`;
+    }
+
+    let from = '';
+    if(this.from.length > 0) {
+      from = ` FROM ${this.from}`;
+    }
+
+    let join = '';
+    if(this.join.length > 0) {
+      join = ` ${this.join.map(j => j).join(' ')}`;
+    }
+
+    let where = '';
+    if(this.where.length > 0) {
+      where = ` HAVING ${this.where}`;
+    }
+
+    let limit = '';
+    if((this.limit || []).length > 0) {
+      limit = ` LIMIT ${this.limit}`;
+    }
+
+    if(this.fatalError) {
+      return {
+        status: 'error',
+        errors: this.errors,
+        query: ''
+      }
+    }
+
+    return {
+      status: 'success',
+      errors: this.errors,
+      query: `${select}${from}${join}${where}${limit}` 
+    }
+  }
+  create(queryObj) {
+    this.parseQueryObj(queryObj);
+  }
+  update(queryObj) {
+    this.parseQueryObj(queryObj);
+  }
+  delete(queryObj) {
+    this.parseQueryObj(queryObj);
+  }
+  // █▀▀█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀█ █▀▀
+  // █░░█ █▄▄█ █▄▄▀ ▀▀█ █▀▀ █▄▄▀ ▀▀█
+  // █▀▀▀ ▀░░▀ ▀░▀▀ ▀▀▀ ▀▀▀ ▀░▀▀ ▀▀▀
+
+  parseJoinObject(joinObj) {
+    const {db, table} = this.parseDbAndTableNames(joinObj.name);
+
+    this.join.push(
+      `LEFT JOIN ${db}.${table} ON ${joinObj.where.map(wh => 
+        typeof wh === 'object' ?
+        wh.map(w => w).join(' OR ')
+        :
+        wh
+      ).join(' AND ')}`
+    )
+
+    const select = `${
+      joinObj.columns.length > 0 ? 
+        joinObj.columns.map(col => {
+
+          if(/^\w+\.\w+$/g.test(col.name)) return this.parseJoinObject(col);
+          let name = this.setNameString(db, table, col.name);
+          if(col.as) {
+            name += ` AS ${col.as}`;
+          }
+          return name
+        }).join() 
+      : 
+      ''
+    }`;
+
+    const as = joinObj.as ? ` AS ${joinObj.as}` : '';
+
+    return `${select}${as}`;
+  }
+
+  parseQueryObj(queryObj) {
+    const {db, table} = this.parseDbAndTableNames(queryObj.name);
+
+    this.select = `${
+      queryObj.columns.length > 0 ? 
+        queryObj.columns.map(col => {
+
+          if(/^\w+\.\w+$/g.test(col.name)) return this.parseJoinObject(col);
+          // Check the db, table and name here because we know that queryObj.name will be the right db and table for this col.name.
+          let name = this.setNameString(db, table, col.name);
+          if(col.as) {
+            name += ` AS ${col.as}`;
+          }
+          return name
+        }).join() 
+      : 
+      ''
+    }`;
+
+    this.from = `${queryObj.name}`
+
+    this.where = (queryObj.where || []).length > 0 ? `${queryObj.where.map(wh => {
+      if(wh.length && typeof wh === 'object') return wh.map(w => w).join(' OR ');
+      return wh;
+    }).join(' AND ')}` : '';
+
+    this.limit = queryObj.limit ? queryObj.limit.map(n => n).join() : '';
+  }
+
+  // █▀▀ ▀▀█▀▀ █▀▀█ ░▀░ █▀▀▄ █▀▀▀ █▀▀
+  // ▀▀█ ░░█░░ █▄▄▀ ▀█▀ █░░█ █░▀█ ▀▀█
+  // ▀▀▀ ░░▀░░ ▀░▀▀ ▀▀▀ ▀░░▀ ▀▀▀▀ ▀▀▀
+
+  setNameString(db, table, name) {
+
+    if(/^\w+\=\>/.test(name)) {
+      return this.funcString(db, table, name);
+    }
+    if(/^\$\w+/.test(name)) {
+      return this.jQExtract(db, table, name);
+    }
+
+    return `${db}.${table}.${name}`;
+  }
+
+  funcString(db, table, name) {
+    const func = name.slice(0, name.indexOf('=>'))
+
+    let columns = name.slice(
+      func.length + 2
+    ).match(
+      /^\w+\=\>|[\$\w.]+|['`"].+['`"]|\\|\+|>=|<=|=>|>|<|-|\*|=/g
+    );
+
+    console.log(columns);
+
+    // If there's a nested function we'll catch it here and re-run funcString.
+    return `${func}(${(columns || []).map(nm => /^\w+\=\>/.test(nm) ? this.funcString(db,table,nm) : nm).join()})`;
+  }
+
+  // ░░▀ █▀▀█ █▀▀ ▀▀█▀▀ █▀▀█ ░▀░ █▀▀▄ █▀▀▀ █▀▀
+  // ░░█ █░░█ ▀▀█ ░░█░░ █▄▄▀ ▀█▀ █░░█ █░▀█ ▀▀█
+  // █▄█ ▀▀▀█ ▀▀▀ ░░▀░░ ▀░▀▀ ▀▀▀ ▀░░▀ ▀▀▀▀ ▀▀▀
+
+  jQExtract(db, table, jQStr) {
+    const regx = /(\$\w+)|(\[\d\])|(\.\w+)|(\[\?[\w\s@#:;{},.!"£$%^&*()/?|`¬\-=+~]*\])/g
+    const matches = jQStr.match(regx);
+    const name = `${db}.${table}.${matches[0].slice(1)}`
+    return `JSON_UNQUOTE(JSON_EXTRACT(${name}, ${this.jQStringMaker(name, matches)}))`;
+  }
+
+  jQStringMaker(name, matches) {
+    let result = matches.reduce((arr, match, i) => {
+      return [...arr, this.jQString(name, match, arr[i-1])];
+    }, []);
+
+    return result[result.length - 1];
+  }
+
+  jQString(name, string, prevString) {
+    let nameReg = /\$\w+/;
+    let index = /\[\d\]/;
+    let target = /\.\w+/;
+    let search = /\[\?[\w\s@#:;{},.!"£$%^&*()/?|`¬\-=+~]*\]/;
+
+    if(nameReg.test(string)) {
+      return `CONCAT("$")`;
+    }
+    if(index.test(string)) {
+      // 'index';
+      return `CONCAT(${prevString}, "${string}")`
+    }
+    if(target.test(string)) {
+      // 'target';
+      return `CONCAT(${prevString}, "${string}")`
+    }
+    if(search.test(string)) {
+      // 'search';
+      string = string.slice(2, -1);
+      return `CONCAT(${prevString}, CONCAT('[',SUBSTR(JSON_SEARCH(JSON_EXTRACT(${name}, "$"),'one','${string}'), 4,LOCATE(']',JSON_SEARCH(JSON_EXTRACT(${name}, "$"), 'one', '${string}'))-4),']'))`;
+    }
+  }
+
+  // █░░█ ▀▀█▀▀ ░▀░ █░░ ░▀░ ▀▀█▀▀ ░▀░ █▀▀ █▀▀
+  // █░░█ ░░█░░ ▀█▀ █░░ ▀█▀ ░░█░░ ▀█▀ █▀▀ ▀▀█
+  // ░▀▀▀ ░░▀░░ ▀▀▀ ▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀ ▀▀▀ ▀▀▀
+
+  parseDbAndTableNames(name) {
+    const dbTable = /^\w+\.\w+$/
+    if(!dbTable.test(name)) {
+      this.fatalError = true;
+      return null;
+    }
+    return {
+      db: name.match(/^\w+|\w+$/g)[0],
+      table: name.match(/^\w+|\w+$/g)[1]
+    }
+  }
+
+  // ▀█░█▀ █▀▀█ █░░ ░▀░ █▀▀▄ █▀▀█ ▀▀█▀▀ ░▀░ █▀▀█ █▀▀▄
+  // ░█▄█░ █▄▄█ █░░ ▀█▀ █░░█ █▄▄█ ░░█░░ ▀█▀ █░░█ █░░█
+  // ░░▀░░ ▀░░▀ ▀▀▀ ▀▀▀ ▀▀▀░ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀▀ ▀░░▀
+
+  validateQueryObject(queryObj) {
+    const {db,table} = this.parseDbAndTableNames(queryObj.name);
+    this.dbTableNames.push({db, table});
+
+    if(!(this.schema[db] || {})[table]) {
+      this.fatalError = true;
+      this.errors.push(`${db}.${table} is not in the schema`);
+      return;
+    }
+    // We have to check the where first so it'll still have the relevent db and table names
+    if(queryObj.where) {
+      // Now to validate the where strings
+      queryObj.where = queryObj.where.filter(wh => {
+        if(this.whereStringValid(wh)) return true;
+        return false;
+      });
+    }
+
+    // Remove all invalid columns from the queryObject.
+    queryObj.columns = queryObj.columns.filter(col => {
+      const twoSelections = /^\w+\.\w+$/;
+      if(twoSelections.test(col.name) && this.dbTableValid(col.name)) {
+        // Now check the nested object.
+        this.validateQueryObject(col);
+        return true;
+      }
+      if(col.as) {
+        return this.nameStringValid(db, table, col.name) && this.plainStringValid(col.as);
+      }
+      return this.nameStringValid(db, table, col.name)
+    });
+
+    return queryObj;
+  }
+
+  nameStringValid(db, table, name) {
+    const dbTableColumn = /^\w+\.\w+\.\w+$/;
+    const tableColumn = /^\w+\.\w+$/;
+    const column = /^\w+$/;
+    const string = /^['"`].+['"`]$/g;
+    const func = /^\w+\=\>/;
+    const all = /\*/g;
+    const jQString = /^\$\w+/;
+
+    if(typeof name === 'number') return true;
+
+    if(jQString.test(name) && this.jQStringValid(db, table, name)) return true;
+    if(all.test(name)) return true;
+    if(func.test(name) && this.funcValid(db, table, name)) return true;
+    if(string.test(name) && this.plainStringValid(name)) return true;
+    if(dbTableColumn.test(name) && this.dbTableColumnValid(name)) return true;
+    if(tableColumn.test(name) && this.tableColumnValid(db, name)) return true;
+    if(column.test(name) && this.columnValid(db, table, name)) return true;
+    return false;
+  }
+
+  jQStringValid(db, table, jQString) {
+    const column = jQString.slice(1, jQString.search(/[\.\[]/));
+    if(!this.columnValid(db, table, column)) return false;
+    return true;
+  }
+
+  whereStringValid(whStr) {
+    const parts = whStr.split(' ');
+    // const parts = whStr.match(/[\$\w.]+|['`"].+['`"]|\\|\+|>=|<=|=>|>|<|-|\*|=/g);
+    let valid = false;
+
+    const dbTableColumn = /^\w+\.\w+\.\w+$/;
+    const twoSelections = /^\w+\.\w+$/;
+    const column = /^\w+$/;
+    const string = /^['"`].+['"`]$/g;
+
+    parts.forEach(part => {
+
+      // if the part meets any of these conditions it will go to true
+      if(this.plainStringValid(part)) {
+        valid = true;
+      }
+      //
+      // TODO: decide what you should do here this validation was causing more
+      // problems than it fixed but it should probably be little more rigerous than it is now.
+      //
+      // if(dbTableColumn.test(part) && this.dbTableColumnValid(part, false)) {
+      //   valid = true;
+      // }
+      // this.dbTableNames.forEach(dbTObj => {
+      //   if(twoSelections.test(part) && this.tableColumnValid(dbTObj.db, part, false)) {
+      //     valid = true;
+      //   }
+      //   if(column.test(part) && this.columnValid(dbTObj.db, dbTObj.table, part, false)) {
+      //     valid = true;
+      //   }
+      // });
+      if(!valid) {
+        this.errors.push(part + ' didnt pass validation');
+        this.fatalError = true;
+      }
+    });
+
+    return valid;
+  }
+
+  funcValid(db, table, name) {
+    const func = name.slice(0, name.indexOf('=>'))
+    let valid = false;
+    const columns = name.slice(func.length + 2).split(' ')
+    columns.forEach(nm => {
+      if(this.nameStringValid(db, table, nm)){
+        valid = true;
+      }
+    });
+    // If there's no args to the fn we might still want to use it.
+    if(columns.length === 1 && (columns[0] || '').length === 0) valid = true;
+    return valid;
+  }
+
+  plainStringValid(string) {
+    const regex = /(drop )|;|(update )|( truncate)/gi;
+    if(regex.test(string)) {
+      this.errors.push('The string \'' + string + '\' is not allowed');
+      this.fatalError = true;
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // █▀▀ █▀▀ █░░█ █▀▀ █▀▄▀█ █▀▀█   ▀█░█▀ █▀▀█ █░░ ░▀░ █▀▀▄ █▀▀█ ▀▀█▀▀ ░▀░ █▀▀█ █▀▀▄
+  // ▀▀█ █░░ █▀▀█ █▀▀ █░▀░█ █▄▄█   ░█▄█░ █▄▄█ █░░ ▀█▀ █░░█ █▄▄█ ░░█░░ ▀█▀ █░░█ █░░█
+  // ▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀ ▀░░░▀ ▀░░▀   ░░▀░░ ▀░░▀ ▀▀▀ ▀▀▀ ▀▀▀░ ▀░░▀ ░░▀░░ ▀▀▀ ▀▀▀▀ ▀░░▀
+
+  dbTableValid(string) {
+    // We don't want to push an error here because this could be a valid table.column
+    let m = string.match(/\w+/g);
+    if(!(this.schema[m[0]] || {})[m[1]]) {
+      return false
+    }
+    return true;
+  }
+
+  dbTableColumnValid(string, pushToErrors = true) {
+    let m = string.match(/\w+/g);
+    if(!((this.schema[m[0]] || {})[m[1]] || {})[m[2]]) {
+      if(pushToErrors) this.errors.push(`${m[0]} ${m[1]} ${m[2]} db, table or column not found in schema`);
+      return false
+    }
+    return true;
+  }
+
+  tableColumnValid(db, string, pushToErrors = true) {
+    let m = string.match(/\w+/g);
+    if(!((this.schema[db] || {})[m[0]] || {})[m[1]]) {
+      if(pushToErrors) this.errors.push(`${db} ${m[0]} ${m[1]} table or column not in schema`);
+      return false
+    }
+    return true;
+  }
+
+  columnValid(db, table, string, pushToErrors = true) {
+    if(!((this.schema[db] || {})[table] || {})[string]) {
+      if(pushToErrors) this.errors.push(`${db} ${table} ${string} column not in schema`);
+      return false
+    }
+    return true;
+  }
+}
---
+++ notes.md
@@ -0,0 +1,168 @@
+```js
+const TableObject = {
+  name: String,
+  where: [String/[String]],
+  columns: [TableObject],
+  as: String
+}
+
+const query = {
+  name: 'bms_booking.booking',
+  where: [
+    'bookingsKey = "123"',
+    'created = "2019"'
+  ],
+  columns: [
+    {name: 'bookingName'},
+    {name: 'created'},
+    {
+      name: 'Biggly.users', 
+      where: [
+        'bms_booking.booking.createdUserKey = userKey'
+      ],
+      columns: [
+        {name: 'accessLevel'},
+        {name: 'concat=>firstName lastName', as: 'fullName'}
+      ]
+    }
+  ]
+}
+```
+These could actually just be inline selects that repeat every time a column item is met.
+```sql
+SELECT
+bookingName,
+created,
+(SELECT accessLevel FROM Biggly.users WHERE bms_booking.booking.createdUserKey = userKey) as accessLevel, 
+-- ^ if no `as` value then use the `name` again.
+(SELECT CONCAT(firstName, " ", lastName) FROM Biggly.users WHERE bms_booking.booking.createdUserKey = userKey) as fullName
+FROM bms_booking.booking
+
+HAVING bookingsKey = "123" AND created = "2019" 
+-- ^ we could use HAVING rather than WHERE as it allows us to be more vague.
+```
+
+```js
+const query = {
+  name: 'bms_booking.booking',
+  where: [
+    'bookingName != "Bad Name"'
+  ]
+}
+
+const query = {
+  name: 'bms_booking.booking',
+  where: [ 'bookingsKey = "123"' ],
+  columns: [
+    {name: 'bookingName'},
+    {name: 'created'},
+    {
+      name: 'Biggly.uploads', 
+      where: [
+        'bms_booking.booking.bookingsKey = bookingsKey'
+      ],
+      columns: [
+        {name: 'count=>*', as: 'uploadCount'}
+      ]
+    }
+  ]
+}
+```
+We could leave much of the details up to the user about whether they put the where assignments in the right order
+or if the full db.table.column names are correctly selected.
+
+That would give me space to simply create shorthands for many of the more laborious aspects of mysql syntax.
+
+We should start by creating a function that deals with a single **TableObject** level. It should create a full SELECT
+query with a WHERE.
+
+Now we have joins working but we'll want to validate all the column selections ect with the schema. The columns should
+be fairly easy because a `name` param is always either a single column or a db.table.
+
+db.table is easy to validate so we do that first. once that's done we save the db.table in a global array of objects {db,table}
+after that we can match up each column against these db and tables depending on where we are in the query.
+
+The `where` strings will be much more difficult to deal with. We'll have to first split the string into blocks and validate each on.
+The blocks will be.
+
+string = '"word"'; // < in this case we validate the string.
+number = '1'; // < no validation needed.
+column = 'word'; // < in this case we get the db.table from the parent name.
+table.column = 'word.word'; // < in this case we get the db from the parent name.
+db.table.column = 'word.word.word'; // < in this case we can just validate straight off.
+math = '=' || '>' || '<' || '>=' ... etc (look them up in mysql)
+
+Divide the string by spaces.
+
+
+
+
+
+
+Once the functions are done I could build custom functions that are mapped by name. They could call from a list of
+custom queries we keep in the api somewhere. 
+
+For example I could make a function for the bookings table calls that
+requires more of a custom query that JSequal can't manage normally. That would mean that we don't need use another
+lambda function for it! All the more custom functions could be stored as query strings in the api then called using
+a specified fn name (eg myfunc=>).
+
+We could even replace normal mysql functions like count so that they work within the syntax of JSequal.
+
+
+Trying to created nested objects in mysql selects does work by using JSON_ARRAYAGG and JSON_OBJECT
+can't select more than a single column from the nested select for some reason, who knows.
+
+I think this finally puts the idea of using nested selects to bed. We can do what we want to achieve using a join though.
+Try to get an object with all the values of a possible set of records inside a join.
+
+
+If I get the bookingDivisions table then group by bookingDivKey and join to the bookings and put the bookingName in an array...
+
+It works!
+
+```sql
+SELECT
+bookingDivisions.bookingDivName,
+JSON_ARRAYAGG(JSON_OBJECT("tmpName", divisionTemplates.tmpName)) as divisionTemplates
+from bookingDivisions
+left join divisionTemplates on bookingDivisions.bookingDivKey = divisionTemplates.bookingDivKey
+
+group by bookingDivisions.bookingDivKey
+```
+This could possibly solve the count problem as well by doing a group by we could count the matching results
+in a row rather than doing a nested select.
+
+Yes it works for the count as well, you just have to add a the joined table.column to the argument and it'll tell
+you how many you want to count by.
+
+We might want to add a group by to every query.
+
+What happens if you join more than one table that has multiple results per row? ANSWER: it breaks :( the count from
+the second join affects the count of the first one and so the arrayagg function is also effected in the same way...
+
+```sql
+SELECT
+JSON_ARRAYAGG(JSON_OBJECT("tmpName", divisionTemplates.tmpName)) as divisionTemplates,
+count(divisionTemplates.tmpName) as tmpsAmt,
+count(bookings.bookingsKey) as bookingAmt,
+bookingDivisions.bookingDivName
+from bookingDivisions
+left join divisionTemplates on bookingDivisions.bookingDivKey = divisionTemplates.bookingDivKey
+left join bookings on bookingDivisions.bookingDivKey = bookings.bookingDivKey
+group by bookingDivisions.bookingDivKey
+```
+So you can only do one join like this at a time, meaning if we wanted to do two or more nested json records like this...well
+we can't.
+
+Shit. Scrub all that. It does work as a nested select...
+```slq
+SELECT
+(select JSON_ARRAYAGG(JSON_OBJECT("tmpName", divisionTemplates.tmpName, "tmpKey", divisionTemplates.tmpKey)) from divisionTemplates where divisionTemplates.bookingDivKey = bookingDivisions.bookingdivKey) as tmpsAmt,
+(select json_arrayagg(json_object("bookingName", bookings.bookingName)) from bookings where bookings.bookingDivKey = bookingDivisions.bookingDivKey) as bookings,
+bookingDivisions.bookingDivName
+from bookingDivisions
+```
+Looks like sub selects are back on the table!
+
+
